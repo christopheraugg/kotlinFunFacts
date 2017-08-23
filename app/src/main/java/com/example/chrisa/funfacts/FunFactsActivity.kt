5e3df7703dbe3d822f7a9346d0986c46b0f19948
chrisa <chrisa39@uw.edu>
2017-08-23 12:30:39 -0700

    updated to kotlin 1.1.4-2. Removed FactBook and ColorWheel classes and used resource strings/colors instead.

--- app/src/main/java/com/example/chrisa/funfacts/FunFactsActivity.kt
+++ app/src/main/java/com/example/chrisa/funfacts/FunFactsActivity.kt
@@ -3,6 +3,7 @@ package com.example.chrisa.funfacts
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_fun_facts.*
+import java.util.*
 
 
 class FunFactsActivity: AppCompatActivity() {
@@ -11,14 +12,16 @@ class FunFactsActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fun_facts)
 
-        val mFacts = FactBook(resources.getStringArray(R.array.fact_strings_array))
-        val mColor = ColorWheel(resources.getIntArray(R.array.fact_colors_array))
+        val mFacts = resources.getStringArray(R.array.fact_strings_array)
+        val mColors = resources.getIntArray(R.array.fact_colors_array)
 
         showFactButton.setOnClickListener {
 
-           val color:Int = mColor.color
+           val randomNumberGenerator = Random()
+           val randomNumber:Int = randomNumberGenerator.nextInt(mFacts.size)
+           val color:Int = mColors[randomNumber]
 
-           factTextView.text = mFacts.mFact
+           factTextView.text = mFacts[randomNumber]
            relativeLayout.setBackgroundColor(color)
            showFactButton.setTextColor(color)
        }
